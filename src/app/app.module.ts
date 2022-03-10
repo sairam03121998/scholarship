25ab894ece1660ae0ea0385705ede58532163a4a
Harsha Javalkar <harshajavalkar28@gmail.com>
2022-03-11 00:57:04 +0530

    en

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';
 
+import { myRoutings } from './app-routing.module';
 import { RegisterComponent } from './register/register.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { ApplicationComponent } from './application/application.component';
-import { DemoComponent } from './demo/demo.component';
-
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    RegisterComponent,
-    LoginComponent,
-    ApplicationComponent,
-    DemoComponent
+    
+    myRoutings,
+         RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     ReactiveFormsModule
   ],
   providers: [],
